5f94e0c5278415665c897a49cee99d28f5a33871
DEMANGE Martin <alextrav.12354@gmail.com>
2026-04-03 12:06:52 +0200

    feat : added the feature where you can get the temperature data from a websocket

--- ressources/scripts/main.js
+++ ressources/scripts/main.js
@@ -13,11 +13,10 @@ var temp_hist = new TemperatureHistory('myChart', 50);
 var temp_hist_int = new TemperatureHistory('myChart__int', 50);
 
 var temp = new Temperature(
--10, 40,
-"case", "funny_sentence", temp_hist,
-"case_int", "funny_sentence_int", temp_hist_int
+  -10, 40,
+  "case", "funny_sentence", temp_hist,
+  "case_int", "funny_sentence_int", temp_hist_int
 );
 
-//setInterval(temp.change_value, 200);
 temp.newRandomArray();
-setInterval(() => temp.change_value(), 200);
+setInterval(() => temp.change_value(), 20000);
--- ressources/scripts/temperature.js
+++ ressources/scripts/temperature.js
@@ -16,7 +16,21 @@ export class Temperature{
     O_sentence_int;
     hist_temp_int;
 
+   S_websocket = "wss://ws.hothothot.dog:9502 ";
+   O_socket = new WebSocket(this.S_websocket);
 
+
+    /**
+     * Constructor of the temperature class.
+     * @param min
+     * @param max
+     * @param temp_box
+     * @param alert_box
+     * @param history_temp
+     * @param temp_box_int
+     * @param alert_box_int
+     * @param history_temp_int
+     */
     constructor(min, max,
             temp_box,     alert_box,     history_temp,
             temp_box_int, alert_box_int, history_temp_int){
@@ -30,19 +44,74 @@ export class Temperature{
         this.O_temp_int = document.getElementById(temp_box_int);
         this.O_sentence_int = document.getElementById(alert_box_int);
         this.hist_temp_int = history_temp_int;
+
+        this.get_data();
     }
 
+    get_data(){
+        this.O_socket.onopen = () => {
+            // the "send" method is mandatory because the socket will close the connection if it doesn't receive any message from the client within a certain time frame.
+            this.O_socket.send("FIFTY THOUSAND FISH!!!!!!!!");
+            console.log("WebSocket connection established.");
+        };
+
+        this.O_socket.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            // get the temperature from the interior sensor
+            console.log("int : " + data.capteurs[0].Valeur);
+
+            //get the temperature from the exterior sensor
+            console.log("ext : " + data.capteurs[1].Valeur);
+
+            // NOTE : see the file "temp.json" for the structure of the data sent by the socket.
+
+            // if (data.Nom === "interieur") {
+            //     // this.A_dummy_data = data.values;
+            //     // this.change_value();
+            //     console.log("ext : " + data)
+            // } else if (data.Nom === "exterieur") {
+            //     // this.A_dummy_data_int = data.values;
+            //     // this.change_value();
+            //     console.log("int : " + data)
+            // }
+
+        };
+
+        this.O_socket.onerror = (error) => {
+            console.error("WebSocket error:", error);
+        };
+
+        this.O_socket.onclose = () => {
+            console.log("WebSocket connection closed.");
+        };
+    }
+
+    /**
+     * Returns a random integer between min (inclusive) and max (exclusive).
+     * @param min The minimum value (inclusive).
+     * @param max The maximum value (exclusive).
+     * @returns {number} A random integer between min and max.
+     */
     getRandomInt(min, max) {
       min = Math.ceil(min);
       max = Math.floor(max);
       return Math.floor(Math.random() * (max - min)) + min;
     }
 
+    /**
+     * Generates two new random arrays of 20 integers each, with values between
+     * I_val_min and I_val_max, and assigns them to A_dummy_data and A_dummy_data_int.
+     */
     newRandomArray(){
         this.A_dummy_data = Array.from({length: 20}, () => this.getRandomInt(this.I_val_min, this.I_val_max));
         this.A_dummy_data_int = Array.from({length: 20}, () => this.getRandomInt(this.I_val_min, this.I_val_max));
     }
 
+    /**
+     * Checks the current temperature values in A_dummy_data and A_dummy_data_int
+     * at indices I_temp and I_temp_int, and updates the class of O_temp and O_temp_int,
+     * as well as the text content of O_sentence and O_sentence_int, based on the temperature thresholds.
+     */
     check_color(){
         let I_value = this.A_dummy_data[this.I_temp];
         let I_value_int = this.A_dummy_data_int[this.I_temp_int];
